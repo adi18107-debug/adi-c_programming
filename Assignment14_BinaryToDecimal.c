f9688a06cd33b0b0294dba8ef6f19c107c2358b0
adi18107-debug <adityaawale18107@gmail.com>
2026-03-26 06:52:45 +0000

    save

---
+++ Assignment14_BinaryToDecimal.c
@@ -0,0 +1,16 @@
+#include <stdio.h>  
+
+void main()  
+{  
+    int num, decimal=0, base=1, rem;  
+    printf("Enter a binary number: ");  
+    scanf("%d", &num);  
+    while (num > 0)  
+    {  
+        rem = num % 10;  
+        decimal = decimal + rem * base;  
+        num = num / 10;  
+        base = base * 2;  
+    }  
+    printf("Decimal equivalent: %d\n", decimal);  
+}
